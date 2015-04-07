18e3bf08b6ed98512e5a3f36c08e3d0207a52fe0
Martin Chaine <chaine.martin@gmail.com>
2015-04-07 14:45:15 +0200

    UT work better with testing material...

---
+++ _test/proto.c
@@ -0,0 +1,16 @@
+/*
+** basic.c for basic in /home/chen/dev/src/github.com/casimir/epinorme/_test
+** 
+** Made by Prof Chen
+** Login   <chen@epitech.eu>
+** 
+** Started on  Wed Mar 11 13:46:46 2015 Prof Chen
+** Last update Tue Apr 07 14:29:40 2015 Prof Chen
+*/
+
+void somefunc(int a, ptr_t b);
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
